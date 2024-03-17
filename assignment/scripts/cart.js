a85abe47e9578e150a21b5f0057f9cdd39ca3499
Tou Yang <tou.yang0101@gmail.com>
2024-03-16 22:31:40 -0500

    cart system

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,9 +1,37 @@
-console.log('***** Cart Functions *****');
+//console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+// Create a global variable named `basket` and set it to an empty array.
+let basket = []; 
+
+/* Create a function called `addItem`. It should:
+- take an input parameter for a string `item`
+- add the new item to the global array `basket`. 
+- return `true` indicating the item was added */
+function addItem(item){
+    basket.push(item); 
+    return true;
+}
+console.log(addItem('rice noodles'));
+console.log(addItem('shrimp'));
 
 
+/*
+- Create a function called `listItems`. It should:
+- loop over the items in the `basket` array
+- console.log each individual item on a new line */
+function listItems() {
+    basket.forEach(item => console.log(item));
+}
+
+/*
+- Create a function called `empty`. It should:
+- reset the `basket` to an empty array
+- do not use `basket = []` to reset the array */
+function empty() {
+    basket.splice(0, basket.length);
+}
 
 
 
